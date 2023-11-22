80b5cb449b0feabf3069a40162b04e2801e8088d
Joan Comas <joan.comas.fdz@gmail.com>
2023-11-22 23:23:06 +0100

    Added second unit test in v2

--- system-reporter-v2.Tests/BusinessLogicTest.cs
+++ system-reporter-v2.Tests/BusinessLogicTest.cs
@@ -8,15 +8,41 @@ public class BusinessLogicTest
     [Fact]
     public void DetermineLogChanges_LastDoesntExist_CreatesNew()
     {
-        var now = DateTime.Now;
-        var e = new HardwareConnectionStateChangedEvent("u1", HardwareConnectionState.DISCONNECTED, now);
+        var expectedHardwarUnitId = "u1";
+        var expectedState = HardwareConnectionState.DISCONNECTED;
+        var expectedStartTime = DateTime.Now;
+        var e = new HardwareConnectionStateChangedEvent(expectedHardwarUnitId, expectedState, expectedStartTime);
         
         var result = BusinessLogic.DetermineLogChanges(e, null);
 
         Assert.True(result.IsT0);
-        Assert.Equal("u1", result.AsT0.Instance.HardwareUnitId);
-        Assert.Equal(HardwareConnectionState.DISCONNECTED, result.AsT0.Instance.State);
-        Assert.Equal(now, result.AsT0.Instance.StartTime);
+        Assert.Equal(expectedHardwarUnitId, result.AsT0.Instance.HardwareUnitId);
+        Assert.Equal(expectedState, result.AsT0.Instance.State);
+        Assert.Equal(expectedStartTime, result.AsT0.Instance.StartTime);
+        Assert.Null(result.AsT0.Instance.EndTime);
+    }
+
+    [Fact]
+    public void DetermineLogChanges_EventIsDisconnectedAndLastHasEndTime_CreatesNew()
+    {
+        var expectedHardwarUnitId = "u1";
+        var expectedState = HardwareConnectionState.DISCONNECTED;
+        var expectedStartTime = DateTime.Now;
+        var e = new HardwareConnectionStateChangedEvent(expectedHardwarUnitId, expectedState, expectedStartTime);
+
+        var yesterday = DateTime.Now.Subtract(TimeSpan.FromDays(1));
+        var now = DateTime.Now;
+        var last = new Disconnection(expectedHardwarUnitId, HardwareConnectionState.WAITING, yesterday)
+        {
+            EndTime = now,
+        };
+
+        var result = BusinessLogic.DetermineLogChanges(e, last);
+
+        Assert.True(result.IsT0);
+        Assert.Equal(expectedHardwarUnitId, result.AsT0.Instance.HardwareUnitId);
+        Assert.Equal(expectedState, result.AsT0.Instance.State);
+        Assert.Equal(expectedStartTime, result.AsT0.Instance.StartTime);
         Assert.Null(result.AsT0.Instance.EndTime);
     }
 }
